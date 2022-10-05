36adee04736f019e5f375c4903cc1b36401197a9
Elena Sinyagina <lenaakimova08@rambler.ru>
2022-10-06 00:31:43 +0400

    Work on mistakes

--- Homework2/Task4.py
+++ Homework2/Task4.py
@@ -2,13 +2,22 @@
 # [-N, N]. Найдите произведение элементов на указанных позициях. 
 # Позиции хранятся в файле file.txt в одной строке одно число.
 
+from random import randint
+
+
 n=int(input('Введите число: '))
 some_list = []
-for i in range (-n, n+1):
-    some_list.append(i)
-with open('file.txt') as file:
-    a = int(file.readline())
-    b = int(file.readline())
+result = 1
+
+for i in range (n):
+    some_list.append(randint(-n, n))
+
+file = open('file.txt', 'r')
+
+for line in file:
+    result *= some_list[int(line)]
+    
+file.close()
+
 print(some_list)
-result = some_list[a] * some_list[b]
 print(result)
