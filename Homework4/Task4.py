016a4321a08e20c3216458644f69112113c8602c
Elena Sinyagina <lenaakimova08@rambler.ru>
2022-10-10 01:07:06 +0400

    HW4 Task 4

--- Homework4/Task4.py
+++ Homework4/Task4.py
@@ -4,3 +4,31 @@
 # Пример:
 # k=2 => 2*x^2 + 4*x + 5 = 0 или x^2 + 5 = 0 или 10*x^2 = 0
 
+from random import randint
+
+
+def func(k):
+    data = open("file.txt", "a")
+    a=int(0)
+    for i in range(-k, -1):
+        a=randint(0, 101)
+        if a != 0:
+            data.write (str(a))
+            data.write ('*x^')
+            data.write (str(-i))
+            data.write ('+')
+    a=randint(0, 101)
+    if a != 0:
+        data.write (str (a))
+        data.write ('*x+')
+    a=randint(0, 101)
+    if a != 0:
+        data.write (str (a))
+        data.write ('=0')
+        data.write('\n')        
+    data.close()
+
+k = int(input('Введите степень k: '))
+
+func(k)
+
