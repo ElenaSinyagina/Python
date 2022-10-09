2a3aa66ece8b8da62574d1ef0270e0bce2ccd647
Elena Sinyagina <lenaakimova08@rambler.ru>
2022-10-10 03:45:18 +0400

    HW4 Task 2  mistake

--- Homework4/Task2.py
+++ Homework4/Task2.py
@@ -1,18 +1,6 @@
 # Задайте натуральное число N. Напишите программу, которая составит 
 # список простых множителей числа N.
 
-# num = int(input())
-# list_simple = []
-# simple = 2
-# while num > 1:
-#     if num % simple == 0:
-#         list_simple.append(simple)
-#         num = num/simple
-#     else:
-#         simple += 1
-# print(list_simple)
-
-# exit(8)
 
 n = int(input('Введите число N: '))
 list = []
